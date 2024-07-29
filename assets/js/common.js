d8fc3d7a670550ddd9ec1f947fb38f494a16b821
hongsik-lee <hslee1166@gmail.com>
2024-07-29 15:17:18 +0900

    merge

--- assets/js/common.js
+++ assets/js/common.js
@@ -120,3 +120,20 @@ const init = () => {
 }
 
 init();
+
+function collapse(element) {
+  var before = document.getElementsByClassName("active")[0] 
+  if (before && document.getElementsByClassName("active")[0] != element) { 
+      before.nextElementSibling.style.maxHeight = null;   
+      before.classList.remove("active");               
+  }
+  element.classList.toggle("active");         
+
+  var content = element.nextElementSibling;
+  if (content.style.maxHeight != 0) {        
+      content.style.maxHeight = null;        
+  } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+  }
+}
+
